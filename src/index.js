8131d5168d7f689a60cc61f263ae7e7411de8d0f
Diego <diegoferreiradoamaral@gmail.com>
2021-07-11 15:21:27 -0300

    Desafio 02 concluido

--- src/index.js
+++ src/index.js
@@ -1,28 +1,77 @@
 const express = require('express');
 const cors = require('cors');
-
 const { v4: uuidv4, validate } = require('uuid');
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not exists!' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  const userFreePlanAvailable = !user.pro && user.todos.length < 10;
+  const userProPlanAvailable = user.pro;
+
+  if (!userFreePlanAvailable && !userProPlanAvailable) {    
+    return response.status(403).json({ error: 'You have reached the limit of your free plan, buy the PRO plan now!' });
+  }
+  
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    return response.status(404).json({ error: 'User not found!' });
+  }
+
+  const idIsValid = validate(id); 
+  if (!idIsValid) {
+    return response.status(400).json({ error: 'The given ID is not valid' });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found!' });
+  }
+
+  request.todo = todo;
+  request.user = user;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+  if (!user) {
+    return response.status(404).json({ error: 'User not found!' })
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
